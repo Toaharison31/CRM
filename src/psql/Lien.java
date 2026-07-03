1e2551617d9c8938459197adf5fc518e9c283f73
Toaharison31 <toaharison@gmail.com>
2026-07-03 09:55:04 +0300

    gitignore

--- src/psql/Lien.java
+++
@@ -1,7 +0,0 @@
-package psql;
-
-public class Lien {
-    public static final String URL = "jdbc:postgresql://localhost:5432/gestion_crm";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "@#3108.anja";
-}
